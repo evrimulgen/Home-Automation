df91f955a6ef7bc8311bb9c44eed9f68e8cf4fa3
James Marino <jamesmarinoau@gmail.com>
2015-11-25 00:03:01 +1100

    Python string to int fix

--- sources/GPIO/TurnOff.py
+++ sources/GPIO/TurnOff.py
@@ -3,13 +3,13 @@ import sys
 
 # Setup
 GPIO.setmode(GPIO.BOARD)
-GPIO.setup(sys.argv[1], GPIO.OUT)
+GPIO.setup(int(sys.argv[1]), GPIO.OUT)
 
 # Turn On
 #GPIO.output(sys.argv[1], True)
 
 # Turn off
-GPIO.output(sys.argv[1], False)
+GPIO.output(int(sys.argv[1]), False)
 
 # Will Reset the GPIO
 GPIO.cleanup()
--- sources/GPIO/TurnOn.py
+++ sources/GPIO/TurnOn.py
@@ -3,10 +3,10 @@ import sys
 
 # Setup
 GPIO.setmode(GPIO.BOARD)
-GPIO.setup(sys.argv[1], GPIO.OUT)
+GPIO.setup(int(sys.argv[1]), GPIO.OUT)
 
 # Turn On
-GPIO.output(sys.argv[1], True)
+GPIO.output(int(sys.argv[1]), True)
 
 # Turn off
 #GPIO.output(sys.argv[1], False)
